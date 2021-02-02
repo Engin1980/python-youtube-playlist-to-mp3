0b6a9754a4491698232fd5108db6312973511571
Marek Vajgl <engin@seznam.cz>
2021-02-02 14:48:26 +0100

    Initial commit

---
+++ downloader.py
@@ -0,0 +1,93 @@
+import datetime
+import time
+
+from pytube import Playlist, YouTube
+
+from track_history import TrackHistory
+
+_verbose = True
+
+
+def _log(*args, **kwargs):
+    if (_verbose):
+        print(*args, **kwargs)
+
+
+def download(url, output, history_old, *, delay_between_tracks=10, verbose=True):
+    global _verbose
+    _verbose = verbose
+    history_new = {}
+    _log("Fetching playlist...")
+    ypl = Playlist(url)
+    track_count = len(ypl.video_urls)
+    _log(f"Found playlist '{ypl.title}' with {track_count} tracks, {len(history_old)} seems to be downloaded already.")
+    _log("Going for videos...")
+    for index, url in enumerate(ypl.video_urls):
+        _log(f"{index + 1}. of {track_count}")
+        if url in history_old.keys():
+            _log(f"\tVideo '{history_old[url].title}' already processed as '{history_old[url].status}', skipping")
+            continue
+        try:
+            yt = YouTube(url)
+            try:
+                (out_file_name, stream_id, abr) = _process_video(yt, output)
+                history_new[url] = _create_history_record_success(yt, stream_id, abr, out_file_name)
+            except Exception as e:
+                _log("\tVideo " + str(index) + " :: " + url + " erroneous, will be not downloaded.")
+                history_new[url] = _create_history_record_fail("Error when downloading stream.", e, yt=yt)
+        except Exception as e:
+            _log("\tVideo " + str(index) + " :: " + url + " erroneous, will be not downloaded.")
+            history_new[url] = _create_history_record_fail("Unable to fetch from YouTube", e, yt=None)
+
+        _log("\tAnti-block-delay...")
+        time.sleep(delay_between_tracks)
+        _log("\t\t... resuming")
+
+    return history_new
+
+
+def _process_video(yt, output_path):
+    _log(f"\ttitle: {yt.title}")
+    _log(f"\tlength: {yt.length} seconds")
+    ys, abr = _get_best_audio_stream(yt)
+    _log("\t\tAudio stream: ", ys)
+    _log("\tDownloading...")
+    final_file_name = ys.download(output_path=output_path)
+    _log("\t... completed")
+    return final_file_name, ys.itag, abr
+
+
+def _get_best_audio_stream(yt):
+    ret = None
+    best_abr = 0
+    for stream in yt.streams:
+        if stream.mime_type != "audio/webm":
+            continue
+        curr_abr = int(stream.abr[:-4])
+        if curr_abr > best_abr:
+            ret = stream
+            best_abr = curr_abr
+    return ret, best_abr
+
+
+def _create_history_record_success(yt, stream_id, abr, output_file_name):
+    track = TrackHistory()
+    track.title = yt.title
+    track.date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    track.stream_id = stream_id
+    track.abr = abr
+    track.target_file = output_file_name
+    track.status = "downloaded"
+
+    return track
+
+
+def _create_history_record_fail(error_info, exception, yt=None):
+    track = TrackHistory()
+    track.title = None if yt is None else yt.title
+    track.date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    track.stream_id = None
+    track.target_file = None
+    track.status = "error: " + error_info + " :: " + str(exception)
+
+    return track
---
+++ main.py
@@ -0,0 +1,110 @@
+import argparse
+import os
+import subprocess
+
+import track_history
+from downloader import download
+
+
+def _read_arguments():
+    parser = argparse.ArgumentParser(description='The script to download audio tracks (webm) from YouTube playlist')
+    parser.add_argument("url",
+                        help="URL of YouTube playlist. Mandatory.")
+    parser.add_argument("output_path", metavar="output-path",
+                        help="Path to the local directory where the result will be saved. Mandatory.")
+    parser.add_argument("--dont-load-history",
+                        help="Do not load the history file from previous downloading.",
+                        action="store_true")
+    parser.add_argument("--dont-save-history",
+                        help="Do not save the history file after current downloading",
+                        action="store_true")
+    parser.add_argument("--delay",
+                        help="Delay between downloads (in seconds). Optional, default 10.",
+                        type=int,
+                        default=10)
+    parser.add_argument("--history-filename",
+                        help="Name of the file where download history is stored. Optional."
+                             " Default: __downloaded.yt.json",
+                        default=track_history.DEFAULT_HISTORY_FILE_NAME)
+    parser.add_argument("--to-mp3",
+                        help="Convert downloaded *.webm files to *.mp3 files and delete *.webm files.",
+                        action="store_true")
+    parser.add_argument("--ffmpeg-path",
+                        help="Path to ffmpeg.exe (PATH variable is used otherwise).")
+    ret = parser.parse_args()
+    return ret
+
+
+def _load_history(args):
+    if args.dont_load_history:
+        ret = {}
+    else:
+        full_history_file_name = os.path.join(args.output_path, args.history_filename)
+        ret = track_history.try_load_track_history(full_history_file_name)
+    return ret
+
+
+def _save_history(args, history):
+    if args.dont_save_history:
+        return
+    else:
+        full_history_file_name = os.path.join(args.output_path, args.history_filename)
+        track_history.save_track_history(history, full_history_file_name)
+
+
+def _merge_history(old, new):
+    ret = []
+    for key in old:
+        ret[key] = old[key]
+    for key in new:
+        ret[key] = new[key]
+    return ret
+
+
+def _convert_to_mp3(args, history):
+    ffmpeg_exe = "ffmpeg.exe" if args.ffmpeg_path is None else args.ffmpeg_path
+    for key in history.keys():
+        item = history[key]
+        if item.status != "downloaded":
+            continue
+        in_file = item.target_file
+        out_file = item.target_file[:-5] + ".mp3"
+        res = subprocess.run([ffmpeg_exe, "-i", in_file, "-vn", "-ab", str(item.abr) + "k", "-y", out_file])
+        if res.returncode == 0:
+            os.remove(in_file)
+
+
+def main():
+    args = _read_arguments()
+    history_old = {} if not args.dont_load_history else _load_history(args)
+    history_new = download(args.url, args.output_path, history_old, verbose=True)
+
+    if args.to_mp3:
+        _convert_to_mp3(args, history_new)
+
+    history = _merge_history(history_old, history_new)
+    if not args.dont_save_history:
+        _save_history(args, history)
+
+
+class OBJ(object):
+    pass
+
+
+def demo():
+    args = OBJ()
+    args.ffmpeg_path = "C:\\Program Files\\net.downloadhelper.coapp\\converter\\build\\win\\64\\ffmpeg.exe"
+    item = OBJ()
+    item.abr = 160
+    item.target_file = "R:\TEMP\\Avril Lavigne - I Will Be.webm"
+    item.status = "downloaded"
+    history = {
+        "a": item
+    }
+
+    _convert_to_mp3(args, history)
+
+
+if __name__ == "__main__":
+    main()
+    # demo()
